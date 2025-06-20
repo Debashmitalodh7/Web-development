62445882b1e00e1ee0269c150a830246eeed0a05
Debashmitalodh <lodhfeni@gmail.com>
2025-06-20 16:51:56 +0600

    Lesson 21 done

--- lesson 19/script2.js
+++ lesson 19/script2.js
@@ -1,8 +1,11 @@
-document.getElementById("heading").innerHTML="Heading from JavaScript";
+document.getElementById("heading").innerHTML = "Heading from JavaScript";
 
 // variables: A container thst holds value
-var number_1 = 45; // data type: number
-var number_2 = 34; // data type: number
+var number_1 = 50; // data type: number
+var number_2 = 70; // data type: number
 var name = "Debashmita"; // data type: string
 
 document.getElementById("add_result").innerHTML = number_1 + number_2;
+
+
+
