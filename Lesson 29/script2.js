2d5e7c15cc9bb022fcd30f0b143e380360030e2d
Debashmitalodh <lodhfeni@gmail.com>
2025-08-23 12:33:52 +0600

    Lesson 29 done

---
+++ Lesson 29/script2.js
@@ -0,0 +1,36 @@
+function validationForm(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("age").value;
+    let message;
+
+
+    if (Name == "") {
+        message = "Name is requried";
+        document.getElementById("validation_message").style.color = "red";
+    }
+
+    else if (age== "") {
+        message = "Age is required";
+        document.getElementById("validation_message").style.color = "red";
+    }
+
+
+    else {
+        message = "Form submitted!";
+        document.getElementById("validation_message").style.color = "green";
+    }
+    document.getElementById("validation_message").innerText = message;
+}
+
+function validateNumber() {
+    const numElement = document.getElementById("number");
+    let message;
+    if (numElement.checkValidity() == false) {
+        message = numElement.validationMessage;
+    } else {
+        message = "OK";
+    }
+    document.getElementById("message").innerText = message;
+}
