e95ef93be86aad58d8ae50f8149511f63c2a65ff
Debashmitalodh <lodhfeni@gmail.com>
2025-10-11 14:40:46 +0600

    Lesson 34 acp done

--- Lesson 34/script2.js
+++ Lesson 34/script2.js
@@ -1,13 +1,26 @@
-function showCalander() {
+function showClock() {
     let date = new Date();
-    let d= date.getDate();
-    let m = date.getMonth();
-    let y = date.getYear();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
 
+    if (h == 0) {
+        h = 12;
+    }
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
 
-var calander = "Date: " + date +"/"+ month +"/"+ year + "Day: " + today;
-document.getElementById("Mycalander").innerText = calander; 
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
+    let time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("clock").innerHTML = time;
+
+    setTimeout(showClock, 1000);
 }
 
-showCalander();
+showClock();
