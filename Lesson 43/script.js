ebd7b183c10174c0845b2a3304f95e3ea361e8bb
Debashmitalodh <lodhfeni@gmail.com>
2025-10-25 17:13:36 +0600

    Lesson 45acp done

---
+++ Lesson 43/script.js
@@ -0,0 +1,18 @@
+function sumALL() {
+    var sum = 0;
+}
+for (i = o; i < arguments.lenght; i++) {
+    sum += arguments[i];
+    return sum;
+}
+
+document.getElementById("demo").innerHTML = sumALL(1, 123, 500, 115, 44, 88);
+
+var value = Math.sin(45);
+document.getElementById("Sin 45 is : " + value + "<br>");
+
+var value1 = Math.cos(45);
+document.getElementById("Cos 45 is : " + value1 + "<br>");
+
+var value2 = Math.tan(45);
+document.getElementById("Tan 45 is : " + value2 + "<br>");   
